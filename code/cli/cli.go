e4ee23782f1b387e1510326b308fb3d27e67a801
Amdre Leon <andrelenin42@gmail.com>
2022-06-19 02:52:22 +0000

    first commit cli imported

--- code/cli/cli.go
+++ code/cli/cli.go
@@ -3,10 +3,21 @@ package main
 import (
 	"agent_platform_client/ap_client"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
+var app = cli.NewApp()
+
 func main() {
-	ap_client.LoadConfig()
-	_, l := ap_client.GetAgentsRequest()
-	fmt.Println(l)
+	err := app.Run(os.Args)
+	if err != nil {
+        log.Fatal(err)
+	} else {
+		ap_client.LoadConfig()
+		_, l := ap_client.GetAgentsRequest()
+		fmt.Println(l)
+	}
 }
